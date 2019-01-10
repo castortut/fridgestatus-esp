3d2a5cfcec2825205f876c215600ad1b1ab60e18
Esa Varemo <esa@kuivanto.fi>
2019-01-10 19:37:41 +0200

    Publish to a device-specific topic

--- motion.lua
+++ motion.lua
@@ -4,7 +4,7 @@ function motion(client)
 
     gpio.mode(IO_PIN, gpio.INPUT)
 
-    last_state = 0
+    last_state = gpio.read(IO_PIN)
 
     t = tmr.create()
     t:register(200, tmr.ALARM_AUTO, function() 
@@ -12,7 +12,7 @@ function motion(client)
         if(last_state == 0 and new_state == 1)
         then
             print("Motion detected")
-            client:publish("/iot/cave/motion1", "detected", 0, 0, function(client) print("sent") end)
+            client:publish("/iot/cave/motion0/" .. node.chipid(), "detected", 0, 0, function(client) print("sent") end)
         end
         last_state = new_state
     end)
